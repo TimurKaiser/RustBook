2e7e309a9cef8803252195aaf11b2a9b27504dca
TimurKaiser <baran.aksoy.pro@gmail.com>
2024-12-09 14:55:45 +0100

    My first Hello World in Rust

---
+++ hello_world/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Hello World !"); // utiliser ! signifie qu'on utilise une macro et non une fonction
+}
+
+// rustc main.rs , pour compiler le fichier
+// rustup component add rustfmt , pour installer le fichier de formatage
+// rustfmt main.rs , pour formater le fichier
+// cargo fmt , à la racine d'un projet pour pouvoir formater un projet
